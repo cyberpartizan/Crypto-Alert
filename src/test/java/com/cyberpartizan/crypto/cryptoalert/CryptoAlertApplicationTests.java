7ddb47aa550e5665f372a66b3ea88ab0ded1d508
cyberpartizan <cyberpartizan@hotmail.com>
2022-01-15 12:01:27 +0300

    Initial commit

---
+++ src/test/java/com/cyberpartizan/crypto/cryptoalert/CryptoAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyberpartizan.crypto.cryptoalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoAlertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
